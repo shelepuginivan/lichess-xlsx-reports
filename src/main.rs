b9fb4a9052412d29698f1af5fdf0d68f76b2c0d5
shelepuginivan <shelepuginivanm@gmail.com>
2026-01-16 23:17:38 +0300

    feat(xlsx): add custom axum response type

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, routing::post};
+use reqwest::StatusCode;
+
+use crate::{data::Data, xlsx::XlsxResponse};
 
 mod data;
 mod lichess;
@@ -6,7 +9,7 @@ mod xlsx;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/api/v1/report", post(generate_report));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
@@ -15,6 +18,6 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    "something"
+async fn generate_report(Json(data): Json<Data>) -> Result<XlsxResponse, StatusCode> {
+    Err(StatusCode::NOT_IMPLEMENTED)
 }
---
+++ src/xlsx/response.rs
@@ -0,0 +1,54 @@
+use std::io::Cursor;
+
+use axum::{
+    http::{StatusCode, header},
+    response::{IntoResponse, Response},
+};
+use umya_spreadsheet::Spreadsheet;
+
+pub struct XlsxResponse {
+    filename: String,
+    spreadsheet: Spreadsheet,
+}
+
+impl XlsxResponse {
+    pub fn new(filename: impl Into<String>, spreadsheet: Spreadsheet) -> Self {
+        Self {
+            filename: filename.into(),
+            spreadsheet,
+        }
+    }
+}
+
+impl IntoResponse for XlsxResponse {
+    fn into_response(self) -> Response {
+        let mut buffer = Cursor::new(Vec::new());
+
+        match umya_spreadsheet::writer::xlsx::write_writer(&self.spreadsheet, &mut buffer) {
+            Ok(_) => {
+                let bytes = buffer.into_inner();
+
+                (
+                    StatusCode::OK,
+                    [
+                        (
+                            header::CONTENT_TYPE,
+                            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        ),
+                        (
+                            header::CONTENT_DISPOSITION,
+                            &format!(r#"attachment; filename="{}""#, self.filename),
+                        ),
+                    ],
+                    bytes,
+                )
+                    .into_response()
+            }
+            Err(e) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to generate spreadsheet: {e}"),
+            )
+                .into_response(),
+        }
+    }
+}
