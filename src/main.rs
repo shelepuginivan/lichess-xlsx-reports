177c8c92e0c1a8431a93324a2c2d6fc8c83ba353
shelepuginivan <shelepuginivanm@gmail.com>
2026-01-16 22:51:48 +0300

    feat: setup basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "something"
 }
