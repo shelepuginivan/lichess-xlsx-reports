1b9f092fcc0c15885d9aff2eddb3dcae8cd06cb8
shelepuginivan <shelepuginivanm@gmail.com>
2026-01-17 17:30:05 +0300

    feat(lichess): handle long game id

--- src/lichess.rs
+++ src/lichess.rs
@@ -3,7 +3,14 @@ use reqwest::Url;
 /// Converts Lichess game URL to PGN export URL.
 pub fn game_url_to_export_url(url: &Url) -> Option<String> {
     let domain = url.domain()?;
-    let game_id = url.path_segments()?.next()?;
+    let mut game_id = url.path_segments()?.next()?;
+
+    // The long game ID can be provided. In this case, the first 8 characters is the actual ID, and
+    // the last 4 is the presentation suffix. The suffix can be safely trimmed, which results in
+    // the analysis board ID.
+    if game_id.len() == 12 {
+        game_id = &game_id[0..8];
+    }
 
     Some(format!(
         "{}://{}/game/export/{}?evals=0&clocks=0",
